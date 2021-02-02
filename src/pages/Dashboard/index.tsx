59e1d53c6bdd190089fad29d3085a32f640dc394
Filipe Dantas <filipedantas90@hotmail.com>
2021-02-02 20:58:40 -0300

    feature dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68872273?s=460&u=665d45599dda816331efa2f4582185246aa9c892&v=4"
+            alt="Filipe Dantas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Teste de repositorio para preencher esse espaço</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68872273?s=460&u=665d45599dda816331efa2f4582185246aa9c892&v=4"
+            alt="Filipe Dantas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Teste de repositorio para preencher esse espaço</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68872273?s=460&u=665d45599dda816331efa2f4582185246aa9c892&v=4"
+            alt="Filipe Dantas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Teste de repositorio para preencher esse espaço</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
