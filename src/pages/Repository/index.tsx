6432e268e04608421b1a7f5c823f9c5937b09af7
Filipe Dantas <filipedantas90@hotmail.com>
2021-02-04 15:47:19 -0300

    feature estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
 }
 
-const Dashboard: React.FC = () => {
+const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <div>Repository:{params.repository}</div>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/68872273?s=460&u=665d45599dda816331efa2f4582185246aa9c892&v=4"
+            alt="Flipdantas"
+          />
+          <div>
+            <strong>flipdantas/primeiro-projeto-react</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="haishis">
+          <div>
+            <strong>ahuhuhu</strong>
+            <p>haiushsaskj</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
-export default Dashboard;
+export default Repository;
